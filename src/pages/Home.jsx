f43934cca1ce825159362089a2123c4530588161
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-13 00:18:57 +0600

    updated home page layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useTheme } from "../context/ThemeProvider";
+import HeroBanner from "../components/HeroBanner";
+import WhyBuildHabits from "../components/WhyBuildHabits";
+import FeaturedHabits from "../components/FeaturedHabits";
+import ExtraSectionOne from "../components/ExtraSectionOne";
+import ExtraSectionTwo from "../components/ExtraSectionTwo";
+
+const Home = () => {
+  const { isDark } = useTheme();
+
+  return (
+    <div
+      className={`min-h-screen transition-colors duration-300 ${
+        isDark
+          ? "bg-linear-to-br from-gray-900 to-gray-800"
+          : "bg-linear-to-br from-[#E5E9C5] to-white"
+      }`}
+    >
+      <HeroBanner />
+      <FeaturedHabits />
+      <WhyBuildHabits />
+      <ExtraSectionOne />
+      <ExtraSectionTwo />
+    </div>
+  );
+};
+
+export default Home;
